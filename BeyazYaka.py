0ee8b50526c58c1ca60755cf7c5ffdd6ce89cdcd
UtkuOzkutucu <utku__ozkutucu@hotmail.com>
2023-06-04 23:10:22 +0300

    Update BeyazYaka.py
    Beyaz Yaka Sıfırdan Yapıldı.

---
+++ BeyazYaka.py
@@ -0,0 +1,30 @@
+from Calisan import Calisan
+class BeyazYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, teşvik_primi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
+        self.__teşvik_primi = teşvik_primi
+        
+    def get_teşvik_primi(self):
+        return self.__teşvik_primi
+    
+    def set_teşvik_primi(self, teşvik_primi):
+        self.__teşvik_primi = teşvik_primi
+        
+    def zam_hakki(self):
+        try:
+            if self.get_tecrube() < 24:
+                return self.__teşvik_primi
+            elif self.get_tecrube() >= 24 and self.get_tecrube() <= 48 and self.get_maas() < 15000:
+                return (self.get_maas() % self.get_tecrube())*5 + self.__teşvik_primi
+            elif self.get_tecrube() > 48 and self.get_maas() < 25000:
+                return (self.get_maas() % self.get_tecrube())*4 + self.__teşvik_primi
+            else:
+                return 0
+        except Exception as Hata: #hatanın ne hatası olduğunu gösterme
+            print("Hata: ", Hata) 
+        
+    def Son_maas(self):
+            return self.get_maas() + self.get_maas() * 0.01 * self.zam_hakki()
+        
+    def __str__(self):
+        return "Ad: {}\nSoyad: {}\nTecrübe: {}\nYeni Maaş: {}\n".format(self.get_ad(), self.get_soyad(), self.get_tecrube(), self.Son_maas())
